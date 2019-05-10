04d66a93829022e2dd3afe12a649a426e382308c
John Oyinbo <j.o.oyinbo@gmail.com>
2019-05-10 13:05:24 +0100

    Delegates; simple arithmetic

---
+++ Delegates/ArithmeticFunction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class Numbers
+    {
+        public  double[] numbers { get; set; }
+    }
+    public class ArithmeticFunction
+    {
+        public static double Add(params double[] numbers)
+        {
+            double result = 0;
+            foreach (var number in numbers)
+            {
+                result += number;
+            }
+            Console.WriteLine($"The addition of all the inputed numbers gives : {result}");
+            return result;
+        }
+        public static double Subtract(params double[] numbers)
+        {
+            double result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result -= numbers[i];
+            }
+            Console.WriteLine($"The Subtraction of all the inputed numbers gives : {result}");
+            return result;
+        }
+        public static double Multiply(params double[] numbers)
+        {
+            double result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result *= numbers[i];
+            }
+            Console.WriteLine($"The Multiplication of all the inputed numbers gives : {result}");
+            return result;
+        }
+        public static double Divide(params double[] numbers)
+        {
+            double result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result /= numbers[i];
+            }
+            Console.WriteLine($"The division of all the inputed numbers gives : {result}");
+            return result;
+        }
+
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate T ArithmeticDelegate<T> (params double[] numbers);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithmeticDelegate<double> arithmeticDelegate = ArithmeticFunction.Add;
+            arithmeticDelegate += ArithmeticFunction.Subtract;
+            arithmeticDelegate += ArithmeticFunction.Divide;
+            arithmeticDelegate += ArithmeticFunction.Multiply;
+            arithmeticDelegate(1,2,3,4);
+            Console.ReadLine();
+        }
+    }
+}
